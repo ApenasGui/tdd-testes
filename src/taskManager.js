9a35fb2c30fe28dd6944abc8b930281cd9823011
ApenasGui <guilherme.s.costa1@gmail.com>
2026-04-01 09:15:51 -0300

    green: implementa createTask e resetId

--- src/taskManager.js
+++ src/taskManager.js
@@ -1,3 +1,9 @@
+let _nextId = 1;
+
+export function resetId(){
+    _nextId = 1;
+}
+
 export function validateTitle(title){
     if(typeof title != 'string'){
         return false;
@@ -5,4 +11,12 @@ export function validateTitle(title){
 
     const titleTrimmed = title.trim();
     return titleTrimmed.length >= 3;
+}
+
+export function createTask(title){
+    return {
+        id: _nextId++,
+        title: title.trim(),
+        completed: false,
+    };
 }
